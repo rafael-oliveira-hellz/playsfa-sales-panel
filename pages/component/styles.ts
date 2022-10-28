14822162fbb27e6aed0047c162e03e4af40eef49
natanboanafina <boanafina.natan@gmail.com>
2022-10-28 20:13:50 -0300

    Styles: Error Message created

--- pages/component/styles.ts
+++ pages/component/styles.ts
@@ -9,7 +9,7 @@ export const Input = styled.div`
     width: 100%;
     height: 3rem;
     margin-top: 1.5rem;
-    margin-bottom: 2rem;
+    margin-bottom: 1.8rem;
 
     & label {
       color: #ffffff;
@@ -37,6 +37,36 @@ export const Input = styled.div`
       input{
         font-size: 0.8rem;
       }
+    }
+  `}
+`;
+
+export const SignUpMessageWrapper = styled.div`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: fit-content;
+    margin-top: -0.5rem;
+    margin-bottom: 1.1rem;
+    padding: 0 1rem;
+
+    & p {
+      color: #ff0000;
+      font-size: 1.2rem;
+      margin-bottom: 6px;
+      text-align: center;
+    }
+
+    & img {
+      width: 10rem;
+      width: 179.5px;
+    }
+
+    & p:hover {
+      text-decoration: underline;
     }
   `}
 `;
