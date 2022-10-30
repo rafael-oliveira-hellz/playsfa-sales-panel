ca7bfb58d3a42011cfd81f7db931708e8b476a36
natanboanafina <boanafina.natan@gmail.com>
2022-10-30 17:28:05 -0300

    Google app image added

--- pages/components/SignUpMessage.tsx
+++ pages/components/SignUpMessage.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import QRCode from '../assets/qr-code-styling.png';
 import SignUpMessageWrapper from './styles/SignUpMessageWrapper.style';
+import AppStore from '../assets/google-play-logo.png';
 
 const SignUpMessage = () => {
   return (
@@ -10,7 +11,17 @@ const SignUpMessage = () => {
           Email inválido! Não possui uma conta? Faça a leitura do nosso QR Code
           e cadastre-se!
         </p>
-        <Image src={QRCode} alt='QR Code para cadastro de novos usuários' />
+        <Image
+          className='toggle-hide-qr'
+          src={QRCode}
+          alt='QR Code para cadastro de novos usuários'
+        />
+        <a
+          className='toggle-hide-app'
+          href='http://link-do-app-na-play-store.com'
+        >
+          <Image src={AppStore} alt='Imagem com link para Google App Store' />
+        </a>
       </SignUpMessageWrapper>
     </>
   );
