901e44a630db173d6b03f9bcd4781a842a7c9a60
Rafael de Oliveira <rafael.oliveirasylva@gmail.com>
2022-10-29 23:09:08 -0300

    chore: minor adjustments

--- pages/components/styles/PurchaseCardWrapper.style.ts
+++ pages/components/styles/PurchaseCardWrapper.style.ts
@@ -1,4 +1,4 @@
-import styled, { css, keyframes } from 'styled-components';
+import styled, { css } from 'styled-components';
 import animation from './Animation.style';
 
 const PurchaseCardWrapper = styled.div`
