b94497cebddd80c28f38704456c64f7be2d3b1b1
natanboanafina <boanafina.natan@gmail.com>
2022-10-30 12:32:16 -0300

    Footer fixed

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,23 +1,20 @@
-import Footer from "./styles/Footer";
+import Footer from './styles/Footer';
 
 const PageFooter = () => {
   return (
-    <Footer className="footer">
-      <div className="footer__container">
-        <div className="footer__container__rights" style={{ color: '#FFF' }}>
-          <p>
-            Play Séries, Filmes Animes
-          </p>
-          <p>
-            © 2022 - Todos os direitos reservados - <em>Desenvolvido por</em>{" "}
-            <a href="https://www.linkedin.com/in/XPTO-7b1b3b1b3/">
-              XPTO
-            </a>
-          </p>
-        </div>
-      </div>
-    </Footer >
+    <Footer>
+      <p>Play Séries, Filmes Animes</p>
+      <p>
+        <em>Desenvolvido por </em>{' '}
+        <a href='https://www.linkedin.com/in/XPTO-7b1b3b1b3/'>
+          Rafael de Oliveira
+        </a>
+        <span>e</span>
+        <a href='https://www.linkedin.com/in/'>Natan Boanafina</a>
+      </p>
+      <p>&#169; 2022 - Todos os direitos reservados</p>
+    </Footer>
   );
-}
+};
 
 export default PageFooter;
--- pages/components/styles/Footer.ts
+++ pages/components/styles/Footer.ts
@@ -6,12 +6,45 @@ const Footer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    align-self: center;
     width: 100%;
-    height: 12rem;
+    height: 8rem;
     background-color: #transparent;
-    position: fixed;
-    bottom: 0;
-    `}
-  `;
+    border-top: 1px solid rgb(51 65 85);
+  `}
+  & p {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    line-height: 1.8;
+  }
+
+  & p:first-child {
+    font-size: 1.5rem;
+  }
+  & p:nth-child(2) {
+    font-size: 1.2rem;
+  }
+  & p:nth-child(2) em {
+    margin-right: 0.3rem;
+  }
+  & p:nth-child(2) span {
+    color: #fff;
+    margin: 0 0.3rem;
+  }
+  & p:nth-child(2) a {
+    text-decoration: underline;
+    transition: all 250ms ease-in-out;
+  }
+
+  & p:nth-child(2) a:hover {
+    color: #005fffff;
+  }
+
+  & p:nth-child(3) {
+    font-size: 1rem;
+  }
+`;
 
 export default Footer;
