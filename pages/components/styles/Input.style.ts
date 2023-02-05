c71de5fdd8e31dd8fbae53d94e8430867d5e2a99
natanboanafina <boanafina.natan@gmail.com>
2023-02-05 16:23:17 -0300

    Some styles fixed

--- pages/components/styles/Input.style.ts
+++ pages/components/styles/Input.style.ts
@@ -9,13 +9,13 @@ const Input = styled.div`
     height: 3.8125rem;
     margin-top: 1.5rem;
     margin-bottom: 1.8rem;
-    background-color: #F9F9FB;
-    border: 3px solid #F9F9FB;
+    background-color: #f9f9fb;
+    border: 3px solid #f9f9fb;
     padding: 0 1rem;
     border-radius: 0.25rem;
 
     & label {
-      color: #1B1B1B;
+      color: #1b1b1b;
       font-size: 1.5rem;
       font-weight: 600;
       text-align: left;
@@ -26,7 +26,7 @@ const Input = styled.div`
       background-color: transparent;
       border: 0;
       outline: 0;
-      color: #1B1B1B;
+      color: #1b1b1b;
       font-weight: 700;
       letter-spacing: 0.125rem;
       font-size: 1.5rem;
@@ -34,24 +34,29 @@ const Input = styled.div`
     }
 
     & input::placeholder {
-      color: #1B1B1B;
+      color: #1b1b1b;
       font-weight: 700;
       letter-spacing: 2px;
     }
 
     @media screen and (max-width: 400px) {
-      & input{
+      & label {
+        font-size: 1.2rem;
+        width: 5rem;
+      }
+
+      & input {
         font-size: 0.8rem;
         width: 75%;
       }
-      & input::placeholder{
+      & input::placeholder {
         font-size: 0.8rem;
         letter-spacing: 0;
         font-weight: 500;
       }
     }
     @media screen and (max-width: 600px) {
-      & input{
+      & input {
         font-size: 0.8rem;
         width: 75%;
       }
