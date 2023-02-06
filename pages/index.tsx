72bbe36bfee467ff5ecdd30773ad56f8315825b2
Rafael de Oliveira (Hellz) <rafael.oliveirasylva@gmail.com>
2023-02-05 23:39:03 -0300

    fix: minor adjustments

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,6 +110,7 @@ export default function Home(data: Props) {
           console.log("Mensagem de Erro de Array? ", Array.isArray(error.response.data));
         } else {
           setErrorMessage(error.response.data.message);
+          setInvalidCpf(false);
         }
       }
     }
