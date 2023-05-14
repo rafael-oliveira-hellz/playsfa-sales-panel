934c1f2c058b0021d4f22632fb6c7f77ab518159
robinhosz <82779533+robinhosz@users.noreply.github.com>
2023-05-13 21:50:19 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
+const redirects = process.env.MAINTENANCE_MODE === "1"
+  ? [{ source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }]
+  : [];
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     styledComponents: true
-  }
+  },
+  redirects,
 };
-
-module.exports = nextConfig;
