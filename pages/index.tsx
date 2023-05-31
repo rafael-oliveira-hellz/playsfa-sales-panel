3e8cc6a06069fcafd7cda2a333dba79fe50f76c4
Rafael de Oliveira <95747012+rafael-oliveira-hellz@users.noreply.github.com>
2023-05-31 13:54:14 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 200px;
+  background-color: #f0f0f0;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const Heading = styled.h1`
+  font-size: 24px;
+  color: #333;
+`;
+
 export default function Home() {
-return <h1>EM MANUTENÇÃO!!!</h1>
+  return (
+    <Card>
+      <Heading>EM MANUTENÇÃO!!!</Heading>
+    </Card>
+  );
 }
