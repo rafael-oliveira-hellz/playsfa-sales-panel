8133f9d530722358cc8e594caecae4a84d048f91
Rafael de Oliveira (Hellz) <rafael.oliveira@infocaltechnologies.com>
2023-06-03 01:14:58 -0300

    fix: validations

--- components/WaitingPayment/styles.ts
+++ components/WaitingPayment/styles.ts
@@ -56,7 +56,7 @@ export const ModalOverlay = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: rgba(0, 0, 0, 0.9);
     z-index: 9999;
     display: flex;
     justify-content: center;
