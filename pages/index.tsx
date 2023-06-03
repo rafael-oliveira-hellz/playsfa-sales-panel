484f8977fbc9768c4bccf56ee7a391e1a34da778
Rafael de Oliveira (Hellz) <rafael.oliveira@infocaltechnologies.com>
2023-06-02 22:13:08 -0300

    chore: remove maintenance mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,320 @@
-import React from 'react';
-import styled from 'styled-components';
+import { LinearProgress } from "@mui/material";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { apiUser, apiUserPix } from "../hooks/api";
+import { Plan } from "../types/Plan";
+import { User } from "../types/User";
+import InputCpf from "./components/CpfInput";
+import InputEmail from "./components/EmailInput";
+import PageFooter from "./components/Footer";
+import PurchaseCard from "./components/PurchaseCard";
+import SignUpMessage from "./components/SignUpMessage";
+import styles from "../styles/Styles.module.css";
+import axios from "axios";
 
-const Container = styled.div`
-  display: grid;
-  place-items: center;
-  height: 100vh;
-`;
+export async function getStaticProps() {
+  const plansUrl = "https://psadns.xyz/plans.php";
 
-const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 300px;
-  height: 200px;
-  background-color: #f0f0f0;
-  border-radius: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
+  const plansResponse = await fetch(plansUrl);
 
-const Heading = styled.h1`
-  font-size: 24px;
-  color: #333;
-  margin-bottom: 10px;
-`;
+  // console.log(plansResponse);
 
-const Subheading = styled.p`
-  font-size: 16px;
-  color: #666;
-`;
+  const plans: Plan[] = await plansResponse.json();
 
-export default function Home() {
+  // console.log(plans);
+
+  return {
+    props: {
+      plans,
+    },
+  };
+}
+
+type Props = {
+  user: User;
+  plans: Plan[];
+};
+
+export default function Home(data: Props) {
+  const [email, setEmail] = useState("");
+  const [plans, setPlans] = useState<Plan[]>();
+  const [user, setUser] = useState<User>();
+  const [paymentLink, setPaymentLink] = useState("");
+  const [pixQR, setPixQR] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [invalidCpf, setInvalidCpf] = useState(false);
+  const [error, setError] = useState(false);
+  const [planChosen, setPlanChosen] = useState("");
+  const [planPrice, setPlanPrice] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("");
+  const [closeModal, setCloseModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const getPaymentLink = async (
+    email: string,
+    planId: number,
+    type: string
+  ) => {
+    try {
+      setLoading(true);
+      const body = { email, planId, type };
+      await apiUser
+        .post("/getUserByEmail", {
+          ...body,
+        })
+        .then((res: any) => {
+          setUser(res.data.data);
+          setPaymentLink(res.data.paymentUrl);
+        });
+    } catch (error: any) {
+      if (error.response) {
+        setError(true);
+      }
+    }
+    setLoading(false);
+  };
+
+  const getPixQR = async (email: string, plan: Plan, cpf: string) => {
+    try {
+      setLoading(true);
+
+      if (!cpf || !email) {
+        setError(true);
+      }
+
+      const body = { email, plan, cpf };
+
+      await apiUserPix
+        .post("/plans/pix/requestData", {
+          ...body,
+        })
+        .then((res: any) => {
+          setUser(res.data.user);
+          setPixQR(res.data.qrcode.qrcode);
+        });
+    } catch (error: any) {
+      if (error.response) {
+        // console.log("Mensagem de Erro: ", error.response);
+
+        setError(true);
+
+        if (Array.isArray(error.response.data)) {
+          setErrorMessage(error.response.data[0].defaultMessage);
+          setInvalidCpf(true);
+          // console.log("Mensagem de Erro de Array? ", Array.isArray(error.response.data));
+        } else {
+          setErrorMessage(error.response.data.message);
+          setInvalidCpf(false);
+        }
+      }
+    }
+    setLoading(false);
+  };
+
+  const handlePlanChosen = (
+    plan_name: string,
+    plan_id: number,
+    user_email: string,
+    payment_type: string,
+    plan_price: string
+  ) => {
+    setPlanChosen(plan_name);
+    setPlanPrice(plan_price);
+    setPaymentMethod(payment_type);
+    getPaymentLink(user_email, plan_id, payment_type);
+    setEmail("");
+    setCloseModal(false);
+  };
+
+  const handlePixPlanChosen = (
+    plan: Plan,
+    plan_name: string,
+    user_email: string,
+    payment_type: string,
+    plan_price: string
+  ) => {
+    setPlanChosen(plan_name);
+    setPlanPrice(plan_price);
+    setPaymentMethod(payment_type);
+    getPixQR(user_email, plan, cpf);
+    setEmail("");
+    setCloseModal(false);
+  };
+
+  const handleClick = () => {
+    setCloseModal(true);
+    setUser(undefined);
+    setPaymentLink("");
+    setPlanChosen("");
+    setPlanPrice("");
+    setPaymentMethod("");
+    setError(false);
+  };
+
+  const breakLine = (str: string) => {
+    const arr = str.split("!");
+    return arr.map((item, index) => {
+      return (
+        <span key={index}>
+          {item}
+          {index !== arr.length - 1 && " !"}
+          {index !== arr.length - 1 && <br />}
+        </span>
+      );
+    });
+  };
+
+  useEffect(() => {
+    setPlans(data.plans);
+  }, [data.plans, error, user]);
+
   return (
-    <Container>
-      <Card>
-        <Heading>EM MANUTENÇÃO!!!</Heading>
-        <Subheading>Desculpe pelo transtorno.</Subheading>
-      </Card>
-    </Container>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Play SFA | Renovar Plano de Streaming</title>
+        <meta
+          name="description"
+          content="Página para renovação de planos de streaming dos usuários da Play Séries, Filmes e Animes"
+        />
+        <link rel="icon" href="/p-icon.svg" />
+      </Head>
+
+      <main id="main" className={styles.main}>
+        <h1 className="font-bold text-5xl mt-5 mb-5">
+          Play Séries, Filmes e Animes
+        </h1>
+        <section
+          id="section"
+          className={`flex flex-col flex-wrap justify-items-center items-center border rounded-2xl border-slate-700 w-11/12 min-h-full ${styles.main_section}`}
+        >
+          <div
+            className={`main_section_cards-wrapper flex flex-col justify-center justify-items-center items-center w-full`}
+          >
+            <InputEmail
+              value={email}
+              autoFocus
+              onChange={(e) => setEmail(e.target.value)}
+              onKeyDown={() => {
+                setError(false);
+              }}
+              placeholder="E-mail"
+              className="email-input"
+            />
+
+            <InputCpf
+              value={cpf}
+              autoFocus
+              onChange={(e) => setCpf(e.target.value)}
+              onKeyDown={() => {
+                setError(false);
+              }}
+              placeholder="(Ex.: 99988877722)"
+              paymentMethod={paymentMethod}
+              className="cpf-input"
+            />
+          </div>
+          {loading ? (
+            <LinearProgress
+              color="secondary"
+              id="progress-bar"
+              style={{
+                marginTop: "-0.5rem",
+                marginBottom: "1rem",
+                width: "55%",
+              }}
+            />
+          ) : null}
+          {error ? (
+            <SignUpMessage
+              invalidCpf={invalidCpf}
+              cpf={paymentMethod === "pix" ? cpf : null}
+              error={paymentMethod === "pix" ? errorMessage : null}
+            />
+          ) : null}
+
+          <div
+            id="map-wrapper"
+            style={{ justifyContent: "space-evenly" }}
+            className={`flex flex-row pb-3 ${styles.main_section_cards_wrapper}`}
+          >
+            {plans &&
+              plans.map((plan) => (
+                <div
+                  className="map-wrapper_div-card border  border-double rounded border-zinc-800 w-2/3 h-auto mx-3 p-2"
+                  key={plan.id}
+                  style={{
+                    backgroundColor: "#ffffff",
+                  }}
+                >
+                  <h2 className="div-card_title text-center font-semibold text-2xl underline mb-2">
+                    {plan.name}
+                  </h2>
+                  <p className="text-left px-2 py-5">
+                    <strong>{breakLine(plan.description)}</strong>
+                  </p>
+                  <div className="flex flex-col justify-start w-full h-fit pr-3 pb-4 my-2 ml-1">
+                    <p className="text-left font-bold ">
+                      Valor: R${plan.pix_price},00
+                    </p>
+                  </div>
+
+                  <div className="flex flex-col w-full h-auto">
+                    <button
+                      onClick={() =>
+                        handlePixPlanChosen(
+                          plan,
+                          plan.name,
+                          email,
+                          "pix",
+                          plan.pix_price
+                        )
+                      }
+                    >
+                      PAGAR COM <em>PIX</em>
+                    </button>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </section>
+
+        {!error && user && (
+          <PurchaseCard
+            customClass={closeModal ? "modal-closed" : ""}
+            onClick={handleClick}
+            userName={user.name}
+            userEmail={user.email}
+            userPremium={user.premuim}
+            planName={planChosen}
+            planPrice={planPrice}
+            paymentMethod={paymentMethod}
+            paymentUrl={paymentLink}
+            pixQR={pixQR}
+          />
+        )}
+
+        <article className="text-center mt-5">
+          <strong
+            className="info"
+            style={{ color: "#911308", fontWeight: 900 }}
+          >
+            Importante:
+          </strong>{" "}
+          Não armazenamos nem compartilhamos nenhum dado e são usados
+          exclusivamente para gerar o link de pagamento.
+        </article>
+
+        <article className={styles.article}>
+          Está com algum problema com pagamento, premium ou outro assunto? Entre
+          em contato com o suporte pelo{" "}
+          <a href="https://discord.gg/app" target="_blank" rel="noreferrer">
+            Discord
+          </a>
+        </article>
+      </main>
+      <PageFooter />
+    </>
   );
 }
