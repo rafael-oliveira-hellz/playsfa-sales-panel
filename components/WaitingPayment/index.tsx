38981c64576212f2401e4e2afbd14e3e0b967aef
Rafael de Oliveira (Hellz) <rafael.oliveira@infocaltechnologies.com>
2023-06-02 21:55:38 -0300

    fix: close modal automatically

--- components/WaitingPayment/index.tsx
+++ components/WaitingPayment/index.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import * as Styled from "./styles";
 import Deadpool from "../../pages/assets/loading/deadpool.gif";
 import Chuck from "../../pages/assets/loading/chuck.gif";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 interface IProps {
   accepted?: boolean;
@@ -11,22 +13,41 @@ export const WaitingPayment = ({
   accepted,
   paymentConfirmationStatus,
 }: IProps) => {
+  const [showModal, setShowModal] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowModal(false);
+      router.push("/checkout");
+    }, 10000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [router]);
+
   return (
     <>
-      {accepted && paymentConfirmationStatus === "PAGAMENTO RECEBIDO" ? (
-        <Styled.WaitingPaymentWrapper>
-          <h2>PAGAMENTO RECEBIDO</h2>
-          <div className="image-wrapper">
-            <Image src={Chuck} alt="chuck" />
-          </div>
-        </Styled.WaitingPaymentWrapper>
-      ) : (
-        <Styled.WaitingPaymentWrapper>
-          <h2>{paymentConfirmationStatus}</h2>
-          <div className="image-wrapper">
-            <Image src={Deadpool} alt="deadpool" />
-          </div>
-        </Styled.WaitingPaymentWrapper>
+      {showModal && (
+        <>
+          {accepted && paymentConfirmationStatus === "PAGAMENTO RECEBIDO" ? (
+            <Styled.WaitingPaymentWrapper>
+              <h2>PAGAMENTO RECEBIDO</h2>
+              <div className="image-wrapper">
+                <Image src={Chuck} alt="chuck" />
+              </div>
+            </Styled.WaitingPaymentWrapper>
+          ) : (
+            <Styled.WaitingPaymentWrapper>
+              <h2>{paymentConfirmationStatus}</h2>
+              <div className="image-wrapper">
+                <Image src={Deadpool} alt="deadpool" />
+              </div>
+            </Styled.WaitingPaymentWrapper>
+          )}
+        </>
       )}
     </>
   );
