5b6e9bf1194521e24a547654f73fd7979ec7502f
Rafael de Oliveira (Hellz) <rafael.oliveira@infocaltechnologies.com>
2023-06-04 10:35:51 -0300

    refactor: clean code

--- components/PixPayment/index.tsx
+++ components/PixPayment/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import * as Styled from "./styles";
 import { Plan } from "../../types/Plan";
 import { UserContextData } from "../../types/User";
@@ -6,63 +6,57 @@ import axios from "axios";
 import { PixPaymentLoading } from "../PixLoader";
 import { connect } from "../../hooks/websocket-client";
 import { WaitingPayment } from "../WaitingPayment";
-
-interface IProps {
+ interface IProps {
   selectedPlan: Plan;
   user: UserContextData;
 }
-
-export const PixPayment = ({ selectedPlan, user }: IProps) => {
+ export const PixPayment = ({ selectedPlan, user }: IProps) => {
   const [cpf, setCpf] = useState("");
   const [qr, setQr] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState({ status: false, message: "", invalidCpf: false });
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(false);
-  const [invalidCpf, setInvalidCpf] = useState(false);
   const [loader, setLoader] = useState(false);
   const [paymentConfirmation, setPaymentConfirmation] = useState("");
   const [confirmed, setConfirmed] = useState(false);
   const [qrcodeReceived, setQrcodeReceived] = useState(false);
   const [showModal, setShowModal] = useState(true);
 
-  const handleCloseModal = () => {
+   const handleCloseModal = () => {
     setShowModal(false);
 
-    if (paymentConfirmation === "ENTREGA DO PREMIUM CONCLUIDA") {
+     if (paymentConfirmation === "ENTREGA DO PREMIUM CONCLUIDA") {
       setTimeout(() => {
         sessionStorage.clear();
         window.location.href = "/";
-      }
-      , 3000);
+      }, 3000);
     }
   };
-
-  const openPixLink = (link: string) => {
+   const openPixLink = (link: string) => {
     const newWindow = window.open(link, "_blank");
     if (newWindow) {
       newWindow.opener = null;
     }
   };
-
-  const generatePix = async (email: string, plan: Plan, cpf: string) => {
+   const generatePix = useCallback(async (email: string, plan: Plan, cpf: string) => {
     try {
       setLoading(true);
-
-      if (!cpf || !email) {
-        setError(true);
+       if (!cpf || !email) {
+        setError({ status: true, message: "", invalidCpf: false });
         return;
       }
 
-      const body = { email, plan, cpf };
+       const body = { email, plan, cpf };
 
-      setLoader(true);
-      setTimeout(async () => {
+       setLoader(true);
+
+      const timer = setTimeout(async () => {
         const res = await axios.post(
           "https://api.comprar.vip/plans/pix/requestData",
           {
             ...body,
           }
         );
+
         setQr(res.data.qrcode.qrcode);
         openPixLink(res.data.qrcode.linkVisualizacao);
         setQrcodeReceived(true);
@@ -70,50 +64,56 @@ export const PixPayment = ({ selectedPlan, user }: IProps) => {
         setLoader(false);
         setShowModal(true);
       }, 3000);
+       return () => {
+        clearTimeout(timer);
+      };
     } catch (error: any) {
       if (error.response) {
         console.error("Mensagem de Erro: ", error.response);
-
-        setError(true);
-
-        if (Array.isArray(error.response.data)) {
-          setErrorMessage(error.response.data[0].defaultMessage);
-          setInvalidCpf(true);
-        } else {
-          setErrorMessage(error.response.data.message);
-          setInvalidCpf(false);
-        }
+         setError({
+          status: true,
+          message: Array.isArray(error.response.data)
+            ? error.response.data[0].defaultMessage
+            : error.response.data.message,
+          invalidCpf: Array.isArray(error.response.data),
+        });
       }
       setLoading(false);
     }
-  };
-
-  useEffect(() => {
+  }, []);
+   useEffect(() => {
     connect((paymentResponse: string) => {
       console.log("Resposta do pagamento recebida: " + paymentResponse);
-
-      setPaymentConfirmation(paymentResponse);
-
-      if (paymentConfirmation === "PAGAMENTO RECEBIDO" || paymentResponse === "PAGAMENTO RECEBIDO" ||
-        paymentConfirmation === "ENTREGA DO PREMIUM EM ANDAMENTO" || paymentResponse === "ENTREGA DO PREMIUM EM ANDAMENTO"
-        || paymentConfirmation === "ENTREGA DO PREMIUM CONCLUIDA" || paymentResponse === "ENTREGA DO PREMIUM CONCLUIDA") {
+       setPaymentConfirmation(paymentResponse);
+       if (
+        paymentConfirmation === "PAGAMENTO RECEBIDO" ||
+        paymentResponse === "PAGAMENTO RECEBIDO" ||
+        paymentConfirmation === "ENTREGA DO PREMIUM EM ANDAMENTO" ||
+        paymentResponse === "ENTREGA DO PREMIUM EM ANDAMENTO" ||
+        paymentConfirmation === "ENTREGA DO PREMIUM CONCLUIDA" ||
+        paymentResponse === "ENTREGA DO PREMIUM CONCLUIDA"
+      ) {
         setConfirmed(true);
-      } else if (paymentConfirmation === "FALHA NA TRANSAÇÃO" || paymentResponse === "FALHA NA TRANSAÇÃO"
-        || paymentConfirmation === "AGUARDANDO CONFIRMAÇÃO DO PAGAMENTO" || paymentResponse === "AGUARDANDO CONFIRMAÇÃO DO PAGAMENTO"
-        || paymentConfirmation === "AGUARDANDO PAGAMENTO" || paymentResponse === "AGUARDANDO PAGAMENTO") {
+      } else if (
+        paymentConfirmation === "FALHA NA TRANSAÇÃO" ||
+        paymentResponse === "FALHA NA TRANSAÇÃO" ||
+        paymentConfirmation === "AGUARDANDO CONFIRMAÇÃO DO PAGAMENTO" ||
+        paymentResponse === "AGUARDANDO CONFIRMAÇÃO DO PAGAMENTO" ||
+        paymentConfirmation === "AGUARDANDO PAGAMENTO" ||
+        paymentResponse === "AGUARDANDO PAGAMENTO"
+      ) {
         setConfirmed(false);
       }
-
     }, "pix", user.user.id.toString());
   }, [paymentConfirmation, confirmed, user.user.id]);
 
-  useEffect(() => {
+   useEffect(() => {
     if (qrcodeReceived) {
       setConfirmed(false);
     }
   }, [qrcodeReceived]);
 
-  return (
+   return (
     <>
       <Styled.PixPaymentWrapper>
         <h2>Formulário para Preenchimento dos dados PIX!</h2>
@@ -127,7 +127,7 @@ export const PixPayment = ({ selectedPlan, user }: IProps) => {
               placeholder="00000000000"
               onChange={(e) => setCpf(e.target.value)}
             />
-            {invalidCpf && <p>CPF inválido</p>}
+            {error.invalidCpf && <p>CPF inválido</p>}
           </label>
           <label htmlFor="mail">
             E-MAIL
@@ -146,8 +146,7 @@ export const PixPayment = ({ selectedPlan, user }: IProps) => {
       ) : (
         <PixPaymentLoading className="closing" />
       )}
-
-      {qrcodeReceived ? (
+       {qrcodeReceived ? (
         confirmed ? (
           <WaitingPayment
             accepted={true}
