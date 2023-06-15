f44d3bd4bb29c71659afadd14e05a6d0d9c8a2fc
Natan Boanafina <natan.boanafina@infocaltechnologies.com>
2023-06-15 17:26:58 -0300

    permission is being fixed

--- components/Hooks/useCopiedState.tsx
+++ components/Hooks/useCopiedState.tsx
@@ -3,16 +3,42 @@ import React, { useState } from "react";
 export const useCopiedState = () => {
   const [isCopied, setIsCopied] = useState(false);
 
-  const handleCopy = async (text: string) => {
-    navigator.clipboard
-      .writeText(text)
-      .then(() => {
-        setIsCopied(true);
+  const handleCopy = (text: string) => {
+    navigator.permissions
+      .query({
+        name: "persistent-storage",
       })
-      .catch((e) => {
-        alert(`Ocorreu um erro! Tente novamente mais tarde. ${e}`);
-        setIsCopied(false);
+      .then((result) => {
+        if (result.state === "granted") {
+          alert("Permissão negada!");
+        } else {
+          navigator.clipboard
+            .writeText(text)
+            .then(() => {
+              setIsCopied(true);
+            })
+            .catch((e) => {
+              alert(`Ocorreu um erro! Tente novamente mais tarde. ${e}`);
+              setIsCopied(false);
+            });
+        }
       });
+    // // Será 'granted', 'denied' or 'prompt':
+    // console.log(permissionStatus.state);
+
+    // // Escuta mudanças ao estado de permissão
+    // permissionStatus.onchange = () => {
+    //   console.log(permissionStatus.state);
+    // };
+    // navigator.clipboard
+    //   .writeText(text)
+    //   .then(() => {
+    //     setIsCopied(true);
+    //   })
+    //   .catch((e) => {
+    //     alert(`Ocorreu um erro! Tente novamente mais tarde. ${e}`);
+    //     setIsCopied(false);
+    //   });
 
     setTimeout(() => {
       setIsCopied(false);
